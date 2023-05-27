9ee741aba6ed20fc6b671d3d56b800210cdac948
Nazrul <heynazrul@gmail.com>
2023-05-27 12:47:42 +0600

    project initial and file structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+    </>
+  );
+}
+
+export default App
