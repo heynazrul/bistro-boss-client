9d64af5852c4009db9c38d6abb67fc3b5c0ff455
Nazrul <heynazrul@gmail.com>
2023-05-30 01:03:15 +0600

    added react helmet for page title

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import './index.css'
-import {  RouterProvider } from 'react-router-dom';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
 import { router } from './Routes/Routes';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className=''>
+    <HelmetProvider>
       <RouterProvider router={router} />
-    </div>
+    </HelmetProvider>
   </React.StrictMode>
 );
