602bc34a8093d3d1b584578568a1c20bcf24d940
Nazrul <heynazrul@gmail.com>
2023-06-06 18:09:52 +0600

    added custom hook with tanstack query to get cart item in header

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,17 @@ import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './providers/AuthProvider';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <HelmetProvider>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </HelmetProvider>
     </AuthProvider>
   </React.StrictMode>
